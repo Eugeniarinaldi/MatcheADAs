3af80fab7989173024bb2dd96782280d56b77bbf
Eugeniarinaldi <maeugenia.rinaldi@gmail.com>
2021-03-17 10:58:29 -0300

    create/primeros modales

--- JS/app.js
+++ JS/app.js
@@ -2,8 +2,68 @@ const emoji = ['🔞', '🎲', '🎰','🎯','🍸'];
 const grilla = document.getElementById("grilla")
 let level = 7;
 
+/********************************************* */
+/****************** MODALES  **************** */
+/******************************************* */
 
-/* GRILLA */
+const Bienvenidos = () =>{
+  swal({
+      title: "¡Bienvenidos!",
+      text: `En MatcheADAs tu objetivo es juntar tres o más ítems del mismo tipo, ya sea en fila o columna. Para eso, selecciona un ítem y a continuación un ítem adyacente para intercambiarlos de lugar.  Si se forma un grupo, esos ítems se eliminarán y ganarás puntos. ¡Sigue armando grupos de 3 o más antes de que se acabe el tiempo!"
+      Controles
+      Click izquierdo:
+      Enter o Espacio: selección
+      Flechas o WASD: movimiento e intercambio
+      `,
+      button: "A Jugar!",
+  })
+  .then(nivel)
+};
+
+
+/* MODAL NUEVO JUEGO */
+
+const nivel = () =>{
+
+  swal("Nuevo juego, selecciona una dificultad", {
+      buttons:{
+      facil: {
+        text: 'Fácil',
+        value: 'facil'
+    },
+    medio: {
+      text: "Medio",
+      value: "medio",
+    },
+    dificil:{
+      text: 'Difícil',
+      value: 'dificil'
+    },
+  },
+  })
+.then((value) => {
+  switch (value) {
+ 
+      case "facil":
+        tablero(9);
+        break;
+   
+      case "medio":
+        tablero (8);
+        break;
+      case "dificil":
+        tablero (7);
+        break;
+      }
+  });
+}
+
+
+
+/************************************************* */
+/********************* GRILLA ******************* */
+/*********************************************** */
+
 
 const tablero = () => {
     grilla.innerHTML = "";
@@ -39,8 +99,9 @@ const changeNivel = ()=>{
 }
 
 const getRandomInt = (min, max)=> {
-    return Math.floor(random * (max - min)) + min;
     let random = Math.random ();
+    return Math.floor(random * (max - min)) + min;
+
 };
 
 const random = getRandomInt(0, 4)
@@ -50,6 +111,5 @@ const obtenerEmoji = console.log(`posicion: ${random}`, emoji[random]);
 /*getRandomInt(0, 4);*/
 
 tablero();
-
 
 
